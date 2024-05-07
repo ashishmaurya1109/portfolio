eb0681c2f223a11abdc127b58645da496ab04c71
ashishmaurya1109 <ashishmaurya1865@gmail.com>
2024-05-07 21:59:12 +0530

    modigied experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>1+ Year Working</small>
+              <small>8+ Months Working</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
